3d47fff467045b1b51e4b435b05e585ed1a17600
AFei <tjtgcyf@gmail.com>
2023-01-28 15:57:48 +0900

    Upload the other which included common functions

---
+++ include/other.h
@@ -0,0 +1,6 @@
+#ifndef OTHER_H_
+#define OTHER_H_
+
+inline float generateRandom(float left, float right);
+
+#endif
---
+++ src/other.cxx
@@ -0,0 +1,11 @@
+#include <random>
+#include "../include/other.h"
+
+inline float generateRandom(float left, float right)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(left, right);
+    auto res = dis(gen);
+    return (float)res;
+}
