8da8f4537bff6dbcc9a84f3c3d602eef0189f12f
AFei <tjtgcyf@gmail.com>
2023-01-28 11:24:42 +0900

    Add the class of Material

--- include/hitable.h
+++ include/hitable.h
@@ -3,6 +3,8 @@
 
 #include "ray.hxx"
 
+class Material;
+
 struct hitRecord
 {
     float t;
@@ -15,6 +17,12 @@ class hiTabel
 {
 public:
     virtual bool hit(AFei::ray<float> &r, float t_min, float t_max, hitRecord &rec) const= 0;
+};
+
+class Material
+{
+public:
+    virtual bool scatter(const AFei::ray<float> &r_in, const hitRecord &rec, AFei::vec3<float> attenuation, AFei::ray<float> &r_out) const = 0;
 };
 
 #endif
