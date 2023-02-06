b38c54bede42a9c1f4fbfebcf4393906d18c0c92
AFei <tjtgcyf@gmail.com>
2023-02-06 20:28:25 +0900

    add Fresnel Effect

--- include/dielectric.hxx
+++ include/dielectric.hxx
@@ -2,6 +2,7 @@
 #define DIELECTRIC_HXX_
 
 #include "hitable.h"
+#include "other.h"
 
 namespace AFei
 {
@@ -19,6 +20,7 @@ namespace AFei
         virtual bool scatter(const AFei::ray<float> &r_in, const hitRecord &rec, AFei::vec3<float> &attenuation, AFei::ray<float> &r_out) const;
         virtual AFei::ray<float> reflect(const AFei::ray<float> &r_in, const hitRecord &rec) const;
         bool refract(const AFei::vec3<float> &r_in_dir, const AFei::vec3<float> r_in_normal, float ni_over_nt, AFei::vec3<float> &r_out_dir) const;
+        float shilick(float cosine, float ref_idx) const;
     };
 }
 
@@ -37,34 +39,55 @@ AFei::dielectirc::dielectirc(float ref_idx)
 bool AFei::dielectirc::scatter(const AFei::ray<float> &r_in, const hitRecord &rec, AFei::vec3<float> &attenuation, AFei::ray<float> &r_out) const
 {
     vec3<float> outward_normal = rec.normal;
-    r_out = reflect(r_in, rec);
+    //auto r_in_temp = r_in;
     float ni_over_nt;
     attenuation = _albedo;
     vec3<float> refracted;
-    
+    float cosine;
+    float reflect_prob;
+
     //光线从玻璃小球内部打入外部
     if(dot(r_in.direction, rec.normal) > 0)
     {
         ni_over_nt = _ref_idx;
         outward_normal = -outward_normal;
+        cosine = dot(r_in.direction, rec.normal);
     }
     //光线从外部打入玻璃小球
     else
     {
         ni_over_nt = 1.0 / _ref_idx;
         outward_normal = rec.normal;
+        cosine = -dot(r_in.direction, rec.normal);
     }
 
+    //发生折射
     if(refract(r_in.direction, outward_normal, ni_over_nt, refracted))
     {
-        r_out = ray<float>(rec.intersectPoint, refracted);
-        return true;
+        reflect_prob = shilick(cosine, _ref_idx);
     }
+    //发生全反射
     else
     {
-        return false;
+        reflect_prob = 1.0f;
     }
 
+    if(generateRandom(0.001f, 1.0f) < reflect_prob)
+    {
+        r_out = reflect(r_in, rec);
+    }
+    else
+    { 
+        r_out = ray<float>(rec.intersectPoint, refracted);
+    }
+    return true;
+}
+
+float AFei::dielectirc::shilick(float cosine, float ref_idx) const
+{
+    float r0 = (1.0f - ref_idx) / (1.0f + ref_idx);
+    r0 = r0 * r0;
+    return r0 + (1.0f - r0) * pow((1.0f - cosine), 5);
 }
 
 AFei::ray<float> AFei::dielectirc::reflect(const AFei::ray<float> &r_in, const hitRecord &rec) const
