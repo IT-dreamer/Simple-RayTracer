0468e655729deddf8fa95b4d1de1aeecdeaf3b27
AFei <tjtgcyf@gmail.com>
2023-02-09 10:02:33 +0900

    Add: fov camera

--- include/camera.hxx
+++ include/camera.hxx
@@ -15,6 +15,7 @@ namespace AFei
     
     public:
         camera();
+        camera(float camera_fov, float camera_aspect);
         camera(vec3<float> camera_origin, vec3<float> camera_lower_left_corner, \
         vec3<float> camera_horizontal, vec3<float> camera_vertical);
     
@@ -29,6 +30,17 @@ AFei::camera::camera()
     this->lower_left_corner = vec3<float>(-2.0f, -1.0f, -1.0f);
     this->horizontal = vec3<float>(4.0f, 0.0f, 0.0f);
     this->vertical = vec3<float>(0.0f, 2.0f, 0.0f);
+}
+
+AFei::camera::camera(float camera_fov, float camera_aspect)
+{
+    float theta  = camera_fov * M_PI / 180.0f;
+    float half_height = tan(theta / 2.0f);
+    float half_width = camera_aspect * half_height;
+    lower_left_corner = vec3<float>(-half_width, -half_height, -1.0f);
+    horizontal = vec3<float>(2.0f * half_width, 0.0f, 0.0f);
+    vertical = vec3<float>(0.0f, 2.0f * half_height, 0.0f);
+    origin = vec3<float>(0.0f, 0.0f, 0.0f);
 }
 
 AFei::camera::camera(vec3<float> camera_origin, vec3<float> camera_lower_left_corner, \
