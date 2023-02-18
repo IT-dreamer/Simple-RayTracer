87176feab0b9fe1496a33b13cacf45ea1076956b
AFei <tjtgcyf@gmail.com>
2023-02-18 11:49:11 +0900

    update: add the random in unit disk function

--- src/other.cxx
+++ src/other.cxx
@@ -1,5 +1,6 @@
 #include <random>
 #include "../include/other.h"
+#include "other.h"
 
 float generateRandom(float left, float right)
 {
@@ -9,6 +10,15 @@ float generateRandom(float left, float right)
     return (float)res;
 }
 
+AFei::vec3<float> randomInUnitDisk()
+{
+    AFei::vec3<float> res;
+    do
+    {
+        res = 2.0f * AFei::vec3<float>(generateRandom(0.0f, 1.0f), generateRandom(0.0f, 1.0f), 0.0f) - AFei::vec3<float>(1.0f, 1.0f, 0.0f);
+    } while (res.squaredLength() > 1.0f);
+    return res;
+}
 AFei::vec3<float> randomInUnitSphere()
 {
     AFei::vec3<float> res;
