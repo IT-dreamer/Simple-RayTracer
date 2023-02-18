15541526c84d6689aadc9c8c7c1d51dc6cbd981a
AFei <tjtgcyf@gmail.com>
2023-02-18 11:50:27 +0900

    update: added zoom camera

--- include/camera.hxx
+++ include/camera.hxx
@@ -2,6 +2,7 @@
 #define CMAERA_H_
 
 #include "ray.hxx"
+#include "other.h"
 
 namespace AFei
 {
@@ -12,6 +13,8 @@ namespace AFei
         vec3<float> lower_left_corner;
         vec3<float> horizontal;
         vec3<float> vertical;
+        vec3<float> u, v, w;
+        float lens_radius;
     
     public:
         camera();
@@ -20,9 +23,12 @@ namespace AFei
         vec3<float> camera_horizontal, vec3<float> camera_vertical);
         camera(vec3<float> lookfrom, vec3<float> lookat, vec3<float> up, float vfov, \
         float aspect);
+        camera(vec3<float> lookfrom, vec3<float> lookat, vec3<float> up, float vfov, \
+        float aspect, float aperture, float focus_dist);
     
     public:
         ray<float> get_ray(float u, float v);
+        ray<float> get_Lens_ray(float u, float v);
     };
 }
 
@@ -56,7 +62,6 @@ AFei::camera::camera(vec3<float> camera_origin, vec3<float> camera_lower_left_co
 
 AFei::camera::camera(vec3<float> lookfrom, vec3<float> lookat, vec3<float> up, float vfov, float aspect)
 {
-    vec3<float> u, v, w;
     float theta = vfov * M_PI / 180.0f;
     float half_height = tan(theta / 2.0);
     float half_width = aspect * half_height;
@@ -69,6 +74,21 @@ AFei::camera::camera(vec3<float> lookfrom, vec3<float> lookat, vec3<float> up, f
     vertical = 2.0f * half_height * v;
 }
 
+AFei::camera::camera(vec3<float> lookfrom, vec3<float> lookat, vec3<float> up, float vfov, float aspect, float aperture, float focus_dist)
+{
+    lens_radius = aperture / 2.0f;
+    float theta = vfov * M_PI / 180.0f;
+    float half_height = tan(theta / 2.0);
+    float half_width = aspect * half_height;
+    origin = lookfrom;
+    w = (lookfrom - lookat).normal_vector();
+    u = cross(up, w).normal_vector();
+    v = cross(w, u);
+    lower_left_corner = origin - half_width * focus_dist * u  - half_height * focus_dist * v  - focus_dist * w;
+    horizontal = 2.0f * half_width * focus_dist * u;
+    vertical = 2.0f * half_height * focus_dist * v;
+}
+
 AFei::ray<float> AFei::camera::get_ray(float u, float v)
 {
     auto ray_dir = lower_left_corner + u * this->horizontal + v * this->vertical - this->origin;
@@ -76,6 +96,13 @@ AFei::ray<float> AFei::camera::get_ray(float u, float v)
     return ray<float>(this->origin, ray_dir);
 }
 
-
+AFei::ray<float> AFei::camera::get_Lens_ray(float s, float t)
+{
+    vec3<float> rd = lens_radius * randomInUnitDisk();
+    vec3<float> offset = this->u * rd.x + this->v * rd.y;
+    auto ray_dir = lower_left_corner + s * this->horizontal + t * this->vertical - this->origin - offset;
+    ray_dir.normalization();
+    return ray(this->origin + offset, ray_dir);
+}
 
 #endif
